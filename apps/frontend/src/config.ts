014a29444a00d90d9f4ad4bd5eb4ebda10ba8012
Tousif <tousif101@outlook.com>
2022-02-17 21:42:32 +0530

    fix(frontend): build

--- apps/frontend/src/config.ts
+++ apps/frontend/src/config.ts
@@ -1,9 +1,5 @@
-import { cleanEnv, str, email } from 'envalid';
-
-const env = cleanEnv(process.env, {
-    // API_KEY: str(),
-    ADMIN_EMAIL: email({ default: 'admin@example.com' }),
-    NODE_ENV: str({ choices: ['development', 'test', 'production', 'staging'] }),
-});
+const env = {
+    NODE_ENV: "development",
+};
 
 export default env;
