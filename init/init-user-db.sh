a3d85980435d1c9e4fb961c3a610b923b0791dc2
Tousif <tousif101@outlook.com>
2022-06-26 01:47:49 +0530

    fix: update

--- init/init-user-db.sh
+++ init/init-user-db.sh
@@ -2,7 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER keycloak WITH PASSWORD 'postgres';
-    CREATE DATABASE keycloak_db;
-    GRANT ALL PRIVILEGES ON DATABASE keycloak_db TO keycloak;
+    CREATE USER hasura WITH PASSWORD 'postgres';
+    CREATE DATABASE hasura_metadata;
+    GRANT ALL PRIVILEGES ON DATABASE hasura_metadata TO hasura;
 EOSQL
