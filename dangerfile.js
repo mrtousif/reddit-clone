dbcb1851002ffda767034a71acf996739b55fa6b
mrtousif <tousif101@outlook.com>
2022-06-26 14:35:10 +0000

    fix: update

--- dangerfile.js
+++ dangerfile.js
@@ -1,5 +1,11 @@
-import { message, danger } from "danger";
+import { message, warn, markdown, danger } from "danger";
 
 const modifiedMD = danger.git.modified_files.join("- ");
 
 message("Changed Files in this PR: \n - " + modifiedMD);
+
+const bigPRThreshold = 600;
+if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
+  warn(':exclamation: Big PR (' + ++errorCount + ')');
+  markdown('> (' + errorCount + ') : Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR will helps faster, easier review.');
+}
