14d6b108c84a4f2106e2c808a573fc540a4de77f
mrtousif <tousif101@outlook.com>
2022-06-26 18:51:07 +0000

    fix: update

--- dangerfile.js
+++ dangerfile.js
@@ -6,8 +6,7 @@ message("Changed Files in this PR: \n - " + modifiedMD);
 
 const bigPRThreshold = 600;
 if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
-  warn(':exclamation: Big PR (' + ++errorCount + ')');
-  markdown('> (' + errorCount + ') : Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR will helps faster, easier review.');
+  warn('Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR will helps faster, easier review.');
 }
 
 const packageChanged = danger.git.modified_files.includes('package.json');
