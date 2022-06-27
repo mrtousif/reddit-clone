ddc7afcbc4936e7788b076c578c29195e37563ff
Tousif <tousif101@outlook.com>
2022-06-28 02:38:03 +0530

    fix: updated dangerfile

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,41 +1,34 @@
-import { message, warn, fail, danger } from "danger";
+import { message, warn, fail, danger, schedule } from "danger";
 import prGuide from "danger-plugin-pr-guide";
-import { jsLockfile, commonValidJson, inCommitGrep } from "danger-plugin-toolbox";
-
-import gifs from "danger-plugin-gifs";
+import coverage from "danger-plugin-coverage";
+import { warnDependencies } from "danger-plugin-node-dependencies";
 
 prGuide();
-jsLockfile();
 
-commonValidJson();
+schedule(coverage());
+warnDependencies();
 
-if (inCommitGrep(/.+\.log$/)) {
-    warn("Do not commit log files");
+const modifiedMD = danger.git.modified_files.join("- ");
+message("Changed Files in this PR: \n - " + modifiedMD);
+
+const bigPRThreshold = 600;
+if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
+    warn(
+        "Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR will helps faster, easier review."
+    );
 }
 
-gifs();
+const packageChanged = danger.git.modified_files.includes("package.json");
+const lockfileChanged = danger.git.modified_files.includes("pnpm-lock.yaml");
+if (packageChanged && !lockfileChanged) {
+    const message = "Changes were made to package.json, but not to pnpm-lock.yaml";
+    const idea = "Perhaps you need to run `pnpm install`?";
+    warn(`${message} - <i>${idea}</i>`);
+}
 
-// const modifiedMD = danger.git.modified_files.join("- ");
-// message("Changed Files in this PR: \n - " + modifiedMD);
-
-// const bigPRThreshold = 600;
-// if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
-//     warn(
-//         "Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR will helps faster, easier review."
-//     );
-// }
-
-// const packageChanged = danger.git.modified_files.includes("package.json");
-// const lockfileChanged = danger.git.modified_files.includes("pnpm-lock.yaml");
-// if (packageChanged && !lockfileChanged) {
-//     const message = "Changes were made to package.json, but not to pnpm-lock.yaml";
-//     const idea = "Perhaps you need to run `pnpm install`?";
-//     warn(`${message} - <i>${idea}</i>`);
-// }
-
-// const pr = danger.github.pr;
-// if (pr.assignee === null) {
-//     fail(
-//         "Please assign someone to merge this PR, and optionally include people who should review."
-//     );
-// }
+const pr = danger.github.pr;
+if (pr.assignee === null) {
+    fail(
+        "Please assign someone to merge this PR, and optionally include people who should review."
+    );
+}
